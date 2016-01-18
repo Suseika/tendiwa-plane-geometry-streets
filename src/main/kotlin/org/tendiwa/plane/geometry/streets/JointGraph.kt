484011537578cb299745c3f79927084ccd24fda7
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-19 01:13:30 +0500

    Make JointGraph a class instead of a function

--- src/main/kotlin/org/tendiwa/plane/geometry/streets/JointGraph.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/streets/JointGraph.kt
@@ -1,25 +1,22 @@
 package org.tendiwa.plane.geometry.streets
 
 import org.jgrapht.UndirectedGraph
-import org.jgrapht.graph.SimpleGraph
+import org.tendiwa.graphs.SimpleGraphWithoutAutoEdges
 import org.tendiwa.graphs.connectivity.connectivityComponents
 import org.tendiwa.plane.geometry.paths.SegmentPath
 import org.tendiwa.plane.geometry.segments.Segment
 
-// TODO: Make this a class that delegates to SimpleGraph
-internal fun JointGraph(
+internal class JointGraph(
     joints: List<Joint>
-): UndirectedGraph<Segment, BiJoint> =
-    SimpleGraph<Segment, BiJoint>({ a, b -> throw IllegalAccessError() })
-        .apply {
-            joints.forEach {
-                it.intergrateInto(this)
-            }
+) : UndirectedGraph<Segment, BiJoint> by SimpleGraphWithoutAutoEdges()
+{
+    init {
+        joints.forEach {
+            it.intergrateInto(this)
         }
+    }
 
-internal val UndirectedGraph<Segment, BiJoint>.paths: List<SegmentPath>
-    get() = connectivityComponents()
-        .map { JointChain(it).toPath() }
-
-
-
+    val paths: List<SegmentPath>
+        get() = connectivityComponents()
+            .map { JointChain(it).toPath() }
+}
